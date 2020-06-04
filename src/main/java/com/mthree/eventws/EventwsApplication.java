b2cebfbdef2de3b60c1df06993154d3b82cfc08c
bhardwajshubh <43513682+bhardwajshubh@users.noreply.github.com>
2020-06-04 21:57:08 +0530

    inital commit

---
+++ src/main/java/com/mthree/eventws/EventwsApplication.java
@@ -0,0 +1,13 @@
+package com.mthree.eventws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventwsApplication.class, args);
+    }
+
+}
